3c8e82559367f761854051835878254bb8aed015
sneha <sneha1423@protonmail.com>
2024-10-17 07:05:15 +0000

    render image carousel

---
+++ src/js/components/singleCarousel.js
@@ -0,0 +1,8 @@
+export default function SingleCarousel(src, active = false) {
+  const div = document.createElement("div");
+  div.classList.add("carousel-item", `${active && "active"}`);
+  div.innerHTML = `
+    <img src="${src}" class="d-block w-100 rounded" alt="...">
+    `;
+  return div;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import Option from "./components/Option";
+import SingleCarousel from "./components/singleCarousel";
 // https://dog.ceo/api/breeds/list/all
 // https://dog.ceo/api/breed/affenpinscher/images/random
+// https://dog.ceo/api/breed/hound/images
 
 //  dom targeting
 const BASE_URL = `https://dog.ceo/api/`;
-const imgEl = document.querySelector("img");
+const carouselConatinerEl = document.querySelector(".carousel-inner");
 const dataBreedList = document.querySelector("#data-breed-list");
 
 //  async function getData(){
@@ -17,21 +19,21 @@ const dataBreedList = document.querySelector("#data-breed-list");
 
 // MARK: fetch
 async function getDogsList() {
-    let breeds = JSON.parse(localStorage.getItem("breeds"));
-  
-    if (!breeds) {
-      try {
-        const res = await fetch(`${BASE_URL}breeds/list/all`);
-        const data = await res.json();
-        localStorage.setItem("breeds", JSON.stringify(data.message));
-        breeds = data.message;
-      } catch (err) {
-        console.error("Error occured", err);
-      }
+  let breeds = JSON.parse(localStorage.getItem("breeds"));
+
+  if (!breeds) {
+    try {
+      const res = await fetch(`${BASE_URL}breeds/list/all`);
+      const data = await res.json();
+      localStorage.setItem("breeds", JSON.stringify(data.message));
+      breeds = data.message;
+    } catch (err) {
+      console.error("Error occured", err);
     }
-  
-    return breeds;
   }
+
+  return breeds;
+}
 // async function getDogsList() {
 
 //   try {
@@ -47,12 +49,13 @@ async function getDogsList() {
 //   //     .catch((err) => console.error("error", err))
 //   //     ;
 // }
-//
-async function getDogsImg(breed) {
+
+//fetch images for a dog breed
+async function getDogsImages(breed) {
   try {
-    const res = await fetch(`${BASE_URL}breed/${breed}/images/random`);
+    const res = await fetch(`${BASE_URL}breed/${breed}/images`);
     const data = await res.json();
-    return data.message;
+    return data.message.slice(0, 10);
   } catch (err) {
     console.error("Error Occured");
   }
@@ -62,9 +65,11 @@ async function getDogsImg(breed) {
   //     .then((data) => data.message);
 }
 
+// MARK:render
+// render multiple breed option for select
 async function renderSelect() {
   const dogList = await getDogsList();
-  const fragment=document.createDocumentFragment()
+  const fragment = document.createDocumentFragment();
   Object.keys(dogList).forEach((dogName) => {
     fragment.appendChild(Option(dogName));
   });
@@ -81,21 +86,35 @@ async function renderSelect() {
 // dataBreedList.appendChild(option);
 renderSelect();
 
-async function renderImage(breed) {
-  imgEl.src = `loading_img.gif`;
-  const dogImg = await getDogsImg(breed);
-  imgEl.src = dogImg;
-  imgEl.alt = breed;
+async function renderImageCarousel(breed) {
+  const data = await getDogsImages(breed);
+
+  const fragment = document.createDocumentFragment();
+
+  data.forEach((link, index) => {
+    fragment.appendChild(SingleCarousel(link, index === 0));
+  });
+  carouselConatinerEl.appendChild(fragment);
+
+  // const singleCarouselEl=SingleCarousel("https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=")
+  // carouselConatinerEl.appendChild(singleCarouselEl)
+  // imgEl.src = `loading_img.gif`;
+  // const dogImg = await getDogsImages(breed);
+  // imgEl.src = dogImg;
+  // imgEl.alt = breed;
   //   getDogsImg(breed).then((data) => {
   //     imgEl.src = data;
   //   });
 }
 
+
 dataBreedList.addEventListener("change", async (e) => {
-  const currentValue = e.target.value;
-  renderImage(currentValue);
+  const currentInput = e.target.value;
+  renderImageCarousel(currentInput);
 });
 
+// initial rendering
 document.addEventListener("DOMContentLoaded", () => {
   renderSelect();
+  renderImageCarousel("affenpinscher")
 });
